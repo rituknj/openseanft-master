b3d5671b573b5651f25b866f64f3d48db91a222c
SAURABH YADAV <rsyadav.abss1@gmail.com>
2022-10-14 11:43:58 +0530

    footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,4 +1,11 @@
 import React from "react";
+import { TbBrandTwitter } from "react-icons/tb";
+import { TbBrandInstagram } from "react-icons/tb";
+import { TbBrandDiscord } from "react-icons/tb";
+import { AiOutlineReddit } from "react-icons/ai";
+import { FiYoutube } from "react-icons/fi";
+import { TbBrandTiktok } from "react-icons/tb";
+import { CgMail } from "react-icons/cg";
 
 function Footer() {
   return (
@@ -6,19 +13,58 @@ function Footer() {
       <div className="container">
         <div className="row">
           <div className="col-lg-6 col-md-6 col-sm-12 col-12">
-            <h3>Stay in the loop</h3>z
+            <h3>Stay in the loop</h3>
             <p>
               Join our mailing list to stay in the loop with our newest feature
               releases, NFT drops, and tips and tricks for navigating OpenSea.
             </p>
             <div class="input-group mb-3">
-            <input type="email" class="form-control" placeholder="Your email address" aria-label="Recipient's username" aria-describedby="button-addon2"/>
-            <button class="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
-          </div>
-          
+              <input
+                type="email"
+                class="form-control"
+                placeholder="Your email address"
+                aria-label="Recipient's username"
+                aria-describedby="button-addon2"
+              />
+              <button
+                class="btn btn-outline-secondary"
+                type="button"
+                id="button-addon2"
+              >
+                Sign Up
+              </button>
+            </div>
           </div>
           <div className="col-lg-6 col-md-6 col-sm-12 col-12">
-         
+            <h3>Join the community</h3>
+            <div>
+              <ul>
+                <li>
+                  <TbBrandTwitter />
+                </li>
+                <li>
+                  <TbBrandInstagram />
+                </li>
+                <li>
+                  <TbBrandDiscord />
+                </li>
+                <li>
+                <AiOutlineReddit />
+              </li>
+                <li>
+                  <TbBrandTwitter />
+                </li>
+                <li>
+                  <FiYoutube />
+                </li>
+                <li>
+                  <TbBrandTiktok />
+                </li>
+                <li>
+                  <CgMail />
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
--- src/Components/Slider1.js
+++ src/Components/Slider1.js
@@ -1,89 +1,89 @@
-// import React, { useRef, useState } from "react";
-// // Import Swiper React components
-// import { Swiper, SwiperSlide } from "swiper/react";
-// import Image1 from "../Components/Images/image-1.avif";
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+import Image1 from "../Components/Images/image-1.avif";
 
-// // Import Swiper styles
-// import "swiper/css";
-// import "swiper/css/pagination";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
 
-// import "./Slider1.css";
+import "./Slider1.css";
 
-// // import required modules
-// import { Autoplay, Pagination, Navigation } from "swiper";
+// import required modules
+import { Autoplay, Pagination, Navigation } from "swiper";
 
-// export default function App() {
-//   return (
-//     <section>
-//       <div className="container">
-//         <Swiper
-//           slidesPerView={1}
-//           spaceBetween={10}
-//           pagination={{
-//             clickable: true,
-//           }}
-//           autoplay={{
-//             delay: 2500,
-//             disableOnInteraction: false,
-//           }}
-//           breakpoints={{
-//             640: {
-//               slidesPerView: 2,
-//               spaceBetween: 20,
-//             },
-//             768: {
-//               slidesPerView: 3,
-//               spaceBetween: 40,
-//             },
-//             1024: {
-//               slidesPerView: 3,
-//               spaceBetween: 50,
-//             },
-//           }}
-//           modules={[Autoplay, Pagination, Navigation]}
-//           className="mySwiper"
-//         >
-//           <SwiperSlide>
-//             <img src={Image1} alt="" />
-//           </SwiperSlide>
-//           <SwiperSlide>
-//             {" "}
-//             <img src={Image1} alt="" />
-//           </SwiperSlide>
-//           <SwiperSlide>
-//             {" "}
-//             <img src={Image1} alt="" />
-//           </SwiperSlide>
-//           <SwiperSlide>
-//             {" "}
-//             <img src={Image1} alt="" />
-//           </SwiperSlide>
-//           <SwiperSlide>
-//             {" "}
-//             <img src={Image1} alt="" />
-//           </SwiperSlide>
-//           <SwiperSlide>
-//             {" "}
-//             <img src={Image1} alt="" />
-//           </SwiperSlide>
-//           <SwiperSlide>
-//             {" "}
-//             <img src={Image1} alt="" />
-//           </SwiperSlide>
-//           <SwiperSlide>
-//             {" "}
-//             <img src={Image1} alt="" />
-//           </SwiperSlide>
-//           <SwiperSlide>
-//             <img src={Image1} alt="" />
-//           </SwiperSlide>
-//         </Swiper>
-//       </div>
-//     </section>
-//   );
-// }
+export default function App() {
+  return (
+    <section>
+      <div className="container">
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={10}
+          pagination={{
+            clickable: true,
+          }}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          breakpoints={{
+            640: {
+              slidesPerView: 2,
+              spaceBetween: 20,
+            },
+            768: {
+              slidesPerView: 3,
+              spaceBetween: 40,
+            },
+            1024: {
+              slidesPerView: 3,
+              spaceBetween: 50,
+            },
+          }}
+          modules={[Autoplay, Pagination, Navigation]}
+          className="mySwiper"
+        >
+          <SwiperSlide>
+            <img src={Image1} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            {" "}
+            <img src={Image1} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            {" "}
+            <img src={Image1} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            {" "}
+            <img src={Image1} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            {" "}
+            <img src={Image1} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            {" "}
+            <img src={Image1} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            {" "}
+            <img src={Image1} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            {" "}
+            <img src={Image1} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={Image1} alt="" />
+          </SwiperSlide>
+        </Swiper>
+      </div>
+    </section>
+  );
+}
 
-import React from "react";
+{/* import React from "react";
 import Video from "../Components/Images/high.mp4";
 
 export default function Slider1() {
@@ -95,3 +95,4 @@ export default function Slider1() {
     </section>
   );
 }
+ */}
